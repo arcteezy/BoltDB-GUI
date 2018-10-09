09236c862414662a9ac785ff1319151cbb6342c3
Rahul <arcteezy@gmail.com>
2018-10-09 19:48:59 +0530

    Update boltDB-gui.go

--- boltDB-gui.go
+++ boltDB-gui.go
@@ -24,6 +24,23 @@ func main() {
 
 }
 
+func getAllBuckets(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Page hit : /writeData")
+	
+	 err = db.View(func(tx *bolt.Tx) error {
+        return tx.ForEach(func(name []byte, _ *bolt.Bucket) error {
+            fmt.Println(string(name))
+            return nil
+        })
+    })
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	
+}
+
 func writeData(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println("Page hit : /writeData")
